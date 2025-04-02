a86b8e4f14e29291180cefaacdce1468147719d6
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-04-02 15:27:52 +0500

    Reorganize exports in gradient_containers_plus.dart to streamline container accessibility and ensure all gradient containers are exported consistently.

--- lib/gradient_containers_plus.dart
+++ lib/gradient_containers_plus.dart
@@ -1,8 +1,7 @@
-export 'gradient_containers_plus/base/gradient_container_base.dart';
-export 'gradient_containers_plus/containers/vertical_gradient_container.dart';
-export 'gradient_containers_plus/containers/radial_gradient_container.dart';
-export 'gradient_containers_plus/containers/sweep_gradient_container.dart';
+// Export all gradient containers
 export 'gradient_containers_plus/containers/animated_gradient_container.dart';
 export 'gradient_containers_plus/containers/glassmorphic_gradient_container.dart';
 export 'gradient_containers_plus/containers/neon_gradient_container.dart';
-// Export other containers as they are created 
+export 'gradient_containers_plus/containers/radial_gradient_container.dart';
+export 'gradient_containers_plus/containers/sweep_gradient_container.dart';
+export 'gradient_containers_plus/containers/vertical_gradient_container.dart'; 
