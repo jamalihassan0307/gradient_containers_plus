2db99713e0172658dff065ef5c6f95392f76485e
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-10-15 07:08:29 +0500

    Add LinearGradientContainer for customizable linear gradient backgrounds

--- lib/gradient_containers_plus.dart
+++ lib/gradient_containers_plus.dart
@@ -2,8 +2,8 @@
 export 'gradient_containers_plus/containers/animated_color_change_container.dart';
 export 'gradient_containers_plus/containers/animated_gradient_container.dart';
 export 'gradient_containers_plus/containers/glassmorphic_gradient_container.dart';
+export 'gradient_containers_plus/containers/linear_gradient_container.dart';
 export 'gradient_containers_plus/containers/neon_gradient_container.dart';
 export 'gradient_containers_plus/containers/radial_gradient_container.dart';
 export 'gradient_containers_plus/containers/sweep_gradient_container.dart';
-export 'gradient_containers_plus/containers/vertical_gradient_container.dart';
 export 'gradient_containers_plus/presets/gradient_presets.dart';
---
+++ lib/gradient_containers_plus/containers/linear_gradient_container.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import '../base/gradient_container_base.dart';
+
+/// A container with a linear gradient background that supports any direction
+class LinearGradientContainer extends GradientContainerBase {
+  /// The colors for the gradient
+  final List<Color> colors;
+
+  /// The stops for the gradient colors
+  final List<double>? stops;
+
+  /// The begin alignment of the gradient
+  final AlignmentGeometry begin;
+
+  /// The end alignment of the gradient
+  final AlignmentGeometry end;
+
+  /// The gradient to be used for the container background.
+  /// If this is provided, the `colors`, `stops`, `begin`, and `end` properties will be ignored.
+  final Gradient? gradient;
+
+  const LinearGradientContainer({
+    super.key,
+    super.child,
+    super.borderRadius,
+    super.width,
+    super.height,
+    super.padding,
+    super.margin,
+    this.colors = const [Colors.blue, Colors.purple],
+    this.stops,
+    this.begin = Alignment.topLeft,
+    this.end = Alignment.bottomRight,
+    this.gradient,
+  });
+
+  @override
+  BoxDecoration buildDecoration(BuildContext context) {
+    return BoxDecoration(
+      gradient: gradient ??
+          LinearGradient(
+            colors: colors,
+            stops: stops,
+            begin: begin,
+            end: end,
+          ),
+      borderRadius: borderRadius ?? BorderRadius.circular(12),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withAlpha(51),
+          blurRadius: 8,
+          offset: const Offset(0, 2),
+        ),
+      ],
+    );
+  }
+}
